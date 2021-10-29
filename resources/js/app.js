220fb2bd1dd7b1f8f6ecb463c85ec1d5b1a56bf4
Jose M Perez <jmperezvzla@gmail.com>
2021-10-28 22:45:44 -0400

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+require('alpinejs');
+
+import { createApp } from 'vue';
+import router from './router'
+
+import CompaniesIndex from './components/companies/CompaniesIndex.vue';
+
+createApp({
+    components: {
+        CompaniesIndex
+    }
+}).use(router).mount('#app')
